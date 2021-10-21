8cf650639abcbe41a03241e86d81210a7acd0620
Teren Sapp <terensapp@gmail.com>
2021-10-21 16:47:03 -0500

    another shot exact

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = ">= 2.19"
+      version = "2.19.5"
     }
   }
   required_version = ">= 0.15"
